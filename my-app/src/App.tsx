47b8ec386a4041cea7e2da0543a4b54271aa6ba8
Dipin <dipinkarunakaran6@gmail.com>
2023-08-25 12:14:50 +0530

    install chakra Ui

---
+++ my-app/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import { Button } from '@chakra-ui/react'
+function App() {
+  
+
+  return (
+    <>
+      <div>
+      <Button colorScheme='blue'>Button</Button>
+      </div>
+     
+    </>
+  )
+}
+
+export default App
